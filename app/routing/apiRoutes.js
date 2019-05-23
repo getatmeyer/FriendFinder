3ac5e84ba87bc322a5e6e6f1084e7cf59975c3d4
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-05-23 14:38:33 -0700

    <push>

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,17 +13,27 @@ app.get("/api/friends", function(req,res){
   
 // Create new input in JSON format 
   app.post("/api/friends", function(req,res){
-    console.log("req: " + req);
-    console.log("res: " + res);
+    console.log("req : ", req.body);
+    // console.log("res: " + res);
+    
+    //data to display the best match
+    var bestFriends = {
+      name: "",
+      photo: "",
+      friendDifference: 100
+    };
 
-    var newInput = function(array) {
-      var newInput = [];
-      for (var i = 0; i < array.length; i++) {
-        newInput.push(parseInt(array[i]));
-        // console.log(newInput);
+
+    // parse array into integers
+
+    // var newInput = function(array) {
+    //   var newInput = [];
+    //   for (var i = 0; i < array.length; i++) {
+    //     newInput.push(parseInt(array[i]));
+    //     console.log(newInput);
         
-      } 
-      }
+    //   } 
+    //   }
     
   })
 }
